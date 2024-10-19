446b97578e65331aae9350dfce0fd7c20d4764c2
eri-botechia <botechia94.erika@gmail.com>
2024-10-18 23:19:47 -0300

    Ãstart index files and set future paths o api-rest, /app for endpoints and /docs for app rest docs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-const startMessage:string="Play on usina web server"
+import dotenv from 'dotenv';
+dotenv.config();
+const port = `${Number(process.env.PORT)||9475}`;
 
-console.log(startMessage)
+import express, {Request, Response} from 'express';
+import path from 'path';
+import cors from 'cors';
+import morgan from 'morgan'
+
+
+const app= express()
+const allowedOrigins = ['http://localhost:9475'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+const indexPath = './../public/index.html'
+
+
+app.use(express.static(path.join(__dirname, './../public/')))
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(indexPath);
+});
+
+app.listen( port, ()=>{
+    console.log(`Server running on port ${port}`);
+})
